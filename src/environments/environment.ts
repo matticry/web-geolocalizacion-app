180e272a320644c8d70b70766047d8b6c687f43c
José Andrés Pacheco Zamora <joseaandrespz@gmail.com>
2025-09-24 17:06:54 -0500

    Commit 24/09/2025
    - cambios en los enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://main.egasyasociados.com:4041/api',
-    apiUrl2: 'http://localhost:5157/api/Geocerca',
+    apiUrl2: 'http://192.168.40.169:7070/api/Geocerca',
 
 };
