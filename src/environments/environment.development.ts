d5b2b6da764bd29ba3758b046de38349b7c2cdbd
José Andrés Pacheco Zamora <joseaandrespz@gmail.com>
2025-09-24 17:08:28 -0500

    Commit 24/09/2025
    - cambios en los enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://main.egasyasociados.com:4041/api',
-    apiUrl2: 'http://localhost:5157/api/Geocerca',
+    apiUrl2: 'http://192.168.40.169:7070/api/Geocerca',
 
     defaultUserImage:
   'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg',
