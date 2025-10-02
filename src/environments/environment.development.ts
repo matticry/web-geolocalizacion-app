f9cf22395bab4ce186495c1dbc974349130a8a4e
José Andrés Pacheco Zamora <joseaandrespz@gmail.com>
2025-10-02 09:43:20 -0500

    Commit 30/09/2025
    - Refactorizacion de codigo, en mapService, se mejoró la visualizacion de los pedidos y cobros.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://main.egasyasociados.com:4041/api',
-    apiUrl2: 'http://192.168.40.169:4041/api/Geocerca',
+    apiUrl2: 'https://main.egasyasociados.com:4041/api/geocercas',
 
     defaultUserImage:
   'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg',
